3a2b1c528439794710ef5ef094f11d8f19fd9c58
blaze.i.burgess@gmail.com <blaze.i.burgess@gmail.com>
2016-07-10 16:22:51 -0400

    took notes as comments to song.rb

--- lib/song.rb
+++ lib/song.rb
@@ -3,11 +3,13 @@ require 'active_support/inflector'
 
 class Song
 
-
+  # depends on the `active_support/inflector` `#pluralize` method
   def self.table_name
     self.to_s.downcase.pluralize
   end
 
+  # gets column data from the db using an internal sqlite method and iterates
+  # through them to pull column names
   def self.column_names
     DB[:conn].results_as_hash = true
 
@@ -21,26 +23,35 @@ class Song
     column_names.compact
   end
 
+  # sets attr_accessors for each column name, which will allow the #initialize method
+  # to work
   self.column_names.each do |col_name|
     attr_accessor col_name.to_sym
   end
 
+  # takes in a hash and iterates through to set each property (instantiated as an attr_accessor
+  # above) to be equal to it's db value
   def initialize(options={})
     options.each do |property, value|
       self.send("#{property}=", value)
     end
   end
 
+  # uses join methods to list every property and value in the correct order and formatting for
+  # a sql insert statement. #table_name_for_insert is a custom method made only for this instance
+  # and is equivalent to self.class.column_names.delete_if {|c| c == 'id'}.join(', ')
   def save
     sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
     DB[:conn].execute(sql)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
 
+  # not necessary but makes the result look cleaner
   def table_name_for_insert
     self.class.table_name
   end
 
+  # formats values for an easy sql insert statement
   def values_for_insert
     values = []
     self.class.column_names.each do |col_name|
@@ -49,6 +60,7 @@ class Song
     values.join(", ")
   end
 
+  # prevents potential mishaps with reassigning the primary key value
   def col_names_for_insert
     self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end
@@ -59,6 +71,3 @@ class Song
   end
 
 end
-
-
-
